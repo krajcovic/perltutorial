dc3cbfa83a41de5508926e517a7d5dc8a59a7a76
Krajcovic <dusan.krajcovic@gmail.com>
2013-03-12 12:53:57 +0100

    3. video

--- helloworld.pl
+++ helloworld.pl
@@ -1,6 +1,28 @@
 #!/usr/bin/perl
 print "Content-type: text/html\n\n";
 
-print "Hello world\n";
+# Variable
+$x = 1;
+
+# List variable
+($x,$y) = (1,2);
+
+$number = 1;
+$string = "Perl tutorial";
+
+print "$number\n";
+print "$string\n";
+
+$x = 1;
+$y = 2;
+$z = $x + $y;
+print "$z\n";
+
+$word1 = "Perl";
+$word2 = "Tutorial";
+
+$string = "$word1 $word2\n";
+print $string;
+
 
 exit;
