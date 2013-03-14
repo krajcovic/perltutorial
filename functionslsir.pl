a98963c43c8629a37b72779398436fd8b36a2bab
Krajcovic <dusan.krajcovic@gmail.com>
2013-03-14 14:22:01 +0100

    20. video

---
+++ functionslsir.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+#
+#	Author: Dusan Krajcovic
+#
+
+use strict;
+use warnings;
+
+print "Content-type: text/html\n\n";
+
+my $string = "thursday";
+my $lenght = length("thursday");
+print "$lenght\n";
+
+$lenght = length("monday");
+print "$lenght\n";
+
+exit;
