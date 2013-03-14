b96ac43f50be0d11694408a09294ffb89777dd1a
Krajcovic <dusan.krajcovic@gmail.com>
2013-03-14 12:12:43 +0100

    6. tutorial from linuxsoft.cz

---
+++ conditionalterms.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+#
+#	Author: Dusan Krajcovic
+#
+
+use strict;
+use warnings;
+
+print "Content-type: text/html\n\n";
+
+print 9 > 6;
+print 9 > 18; print "\n";
+
+print print ""; print "\n";
+
+$a = 3;
+$b = 2;
+
+if($a > $b)
+{
+	print '$a je vetsi nez $b'; print "\n";
+
+	print "$a je vetsi nez $b"; print "\n";
+}
+
+if($a = 0)
+{
+	print "TRUE\n";
+} else {
+	print "FALSE\n";
+}
+
+exit;
