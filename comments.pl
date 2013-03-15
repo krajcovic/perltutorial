28f089d8195d7b615ffadfe7f455ed33413c2ed1
Krajcovic <dusan.krajcovic@gmail.com>
2013-03-15 09:25:07 +0100

    23. video

---
+++ comments.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+#
+#	Author: Dusan Krajcovic
+#
+
+use strict;
+use warnings;
+
+print "Content-type: text/html\n\n";
+
+my $x = 1;
+my $y = 1;
+
+# if statement for $x & $y
+if ($x == $y) {
+	print "Do something\n";	#print
+} # if
+else {
+	print "Do something else\n";
+} #else
+
+################################################################################
+# Pick random number
+################################################################################
+
+
+################################################################################
+# My superduper routine
+################################################################################
+
+exit;
