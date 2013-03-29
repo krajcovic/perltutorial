c9d816f2da48e8cf6c38c40076a8fda9458f74e5
Krajcovic <dusan.krajcovic@gmail.com>
2013-03-29 10:59:13 +0100

    41. video

--- 30-59/environmental.pl
+++ 30-59/environmental.pl
@@ -8,6 +8,14 @@ use warnings;
 
 print "Content-type: text/html\n\n";
 
+foreach my $key (sort (keys %ENV)) {
+	print "$key = $ENV{$key}\n";
+}
 
+my $Query = $ENV{QUERY_STRING};
+my $Referer = $ENV{HTTP_REFERER};
+my $IP = $ENV{REMOTE_ADDR};
+my $Host = $ENV{REMOTE_HOST};
+my $UserAgent = $ENV{HTTP_USER_AGENT};
 
 exit;
