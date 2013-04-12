a40a2b4f779ad463081379b5690c1bd5424e31ed
Krajcovic <dusan.krajcovic@gmail.com>
2013-04-12 15:02:52 +0200

    53. video

--- 30-59/genHTMLFly.pl
+++ 30-59/genHTMLFly.pl
@@ -10,10 +10,22 @@ use CGI;
 use CGI::Carp;
 
 my $cgi = CGI->new();
-print $cgi->header, 
-$cgi->start_html(-title=>'My Page Title'),
-$cgi->h1('My Page Header'),
-"A line of text.",
-$cgi->end_html;
+print $cgi->header,
+
+  $cgi->start_html(
+	-title => 'My Page Title',
+	-meta  => { 'keywords' => 'kw1, kw2', 'description' => 'My description.' },
+	-bgcolor => 'yellow',
+	-vlink   => 'orange'
+  ),
+
+  $cgi->center( $cgi->h1('My Page Header') ), "\n",
+  "A line of text .",
+  $cgi->br, "\n",
+  $cgi->br, "\n",
+  $cgi->img( { -src => 'fish.jpeg', -alt => 'A fish' } ),
+  $cgi->br, "\n",
+  $cgi->a( { -href => 'http://www.scriptsocket.com/', -target => '_blink'}, "ScriptSocket.com" ),
+  $cgi->end_html;
 
 exit;
